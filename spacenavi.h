b320e7ee1451d6f35114a74a5f0b8450ebf5714a
Robert Haschke <rhaschke@techfak.uni-bielefeld.de>
2010-05-12 11:13:57 +0000

    added some comments, expose snavi_is_calibrated

--- spacenavi.h
+++ spacenavi.h
@@ -25,21 +25,26 @@ typedef struct snavi_event {
    int axes[6];
 } snavi_event_t;
 
-/**
-	Right now, only O_NONBLOCK flag is supported (or no flags
-	at all).
-*/
+/* open the given device, supported flags: O_NONBLOCK */
 void* snavi_open (const char* pcName, int flags);
 void  snavi_close(void* dev);
 int   snavi_get_fd(void* dev);
 
+/* set status of internal LED */
 int  snavi_set_led (void* dev, int led_state);
+/* retrieve an event */
 int  snavi_get_event (void* dev, snavi_event_t* event);
 
+/* set deadzone threshold */
 void snavi_set_threshold(void* v, unsigned int iDelta);
 unsigned int snavi_get_threshold(void* v);
 
+/* estimate a software calibration in case, the hardware calibration fails */
 void snavi_calibrate(void* v);
+
+/* return true, if calibration offset is zero,
+	i.e. the device's internal calibration is ok */
+int snavi_is_calibrated(void* v);
 
 #ifdef __cplusplus
 }
